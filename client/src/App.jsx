ff64f01fb808c11921a879ed853b4c3ce4e8ce50
Hayat <rafiehayat5@gmail.com>
2023-04-09 12:01:54 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Login from "./pages/home/login/Login";
+import Register from "./pages/home/register/Register";
+import Watch from "./pages/watch/Watch";
+
+const App = () => {
+  return <Login/>
+};
+
+export default App;
